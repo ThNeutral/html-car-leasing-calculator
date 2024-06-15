012a303c248deb9c4743af907ae5f38ab97c13b4
ThNeutral <gunak47ua@gmail.com>
2024-06-15 14:46:22 +0300

    Finished UI

--- scripts-js/index.js
+++ scripts-js/index.js
@@ -1,3 +1,9 @@
 "use strict";
-let test = document.getElementsByClassName("test")[0];
-test.textContent = "bruh";
+// Generate options for lease-period-dropdown
+const leasePeriodDropdown = document.getElementById("lease-period-dropdown");
+let leasePeriodDropdownInnerHTML = "";
+for (let i = 0; i < 5; i++) {
+    let optionNumber = (i + 1) * 12;
+    leasePeriodDropdownInnerHTML += `<option name=${optionNumber}>${optionNumber}</option>\n`;
+}
+leasePeriodDropdown.innerHTML = leasePeriodDropdownInnerHTML;
--- scripts-ts/index.ts
+++ scripts-ts/index.ts
@@ -1,2 +1,9 @@
-let test: Element = document.getElementsByClassName("test")[0]!
-test.textContent = "bruh" 
+// Generate options for lease-period-dropdown
+const leasePeriodDropdown = document.getElementById("lease-period-dropdown")!;
+let leasePeriodDropdownInnerHTML = "";
+for (let i = 0; i < 5; i++) {
+    let optionNumber = (i + 1) * 12;
+    leasePeriodDropdownInnerHTML += `<option name=${optionNumber}>${optionNumber}</option>\n`
+}
+leasePeriodDropdown.innerHTML = leasePeriodDropdownInnerHTML;
+
